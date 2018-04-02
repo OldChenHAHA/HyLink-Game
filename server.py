d6e7452617acfc00e8d3d3e65bdff93d1d021ed0
OldChen <chen_zhongyao@163.com>
2018-04-02 15:08:54 +0800

    server updated

---
+++ server.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+# 文件名：server.py
+from __future__ import print_function
+import socket               # 导入 socket 模块
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         # 创建 socket 对象
+host = socket.gethostname() # 获取本地主机名
+port = 12345                # 设置端口
+s.bind((host, port))        # 绑定端口
+
+s.listen(5)                 # 等待客户端连接
+while True:
+    c, addr = s.accept()     # 建立客户端连接。
+    print ('Connected address：', end='')
+    print (addr)
+    
+    c.send('Welcome to RPi TCP server !')
+    
+    
+    
+    print()
+    c.close()                # 关闭连接
